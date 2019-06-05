f1e54d75ad3b94d5e018d095871acb71569a539a
divyaraod <divyarao.devaraneni@gmail.com>
2019-06-05 20:30:10 +1000

    Horsedev.sln

---
+++ Horsedev/ClassLibrary1/Pages/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace ClassLibrary1
+{
+    internal class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        IWebElement username => driver.FindElement(By.Id("UserName"));
+        IWebElement password => driver.FindElement(By.Id("Password"));
+        IWebElement loginbtn => driver.FindElement(By.XPath("//input[@value='Log in']"));
+        IWebElement hellohari => driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+        internal void LoginSuccess()
+        {
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+            driver.Manage().Window.Maximize();
+            username.SendKeys("hari");
+            password.SendKeys("123123");
+            loginbtn.Click();
+            Assert.That(hellohari.Text,Is.EqualTo("Hello hari!"));
+        }
+
+        internal void LoginFailure()
+        {
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+            username.SendKeys("haris");
+            password.SendKeys("12312");
+            loginbtn.Click();
+        }
+    }
+
+    
+}
---
+++ Horsedev/ClassLibrary1/TimenMaterial.feature
@@ -0,0 +1,9 @@
+﻿Feature: TimenMaterial
+	Create,Edit,Delete and Validate Time and Material records
+
+@mytag
+Scenario: Create and Validate new record
+	Given I am in the Horsedev Portal
+	And I have Clicked Administration and Time and Material Tab
+	When I Click CreateNew Button and Enter valid data
+	Then I should be able to Create new record and Validate 
